3c97d54c5b4ab51084978bdd551a4443b51251cf
wenju <ProfessorWenJu@users.noreply.github.com>
2018-12-06 16:22:19 +0800

    点击app跳转系统开发者界面

---
+++ app/src/main/java/com/wenju/androidjumpdeveloperview/MainActivity.java
@@ -0,0 +1,16 @@
+package com.wenju.androidjumpdeveloperview;
+
+import android.content.Intent;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS));
+        finish();
+    }
+}
